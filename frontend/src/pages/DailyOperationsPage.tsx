cfaaf3ce37541b6b510ce593f17fe1d3827f151d
Joseph <a20170587@pucp.edu.pe>
2023-05-08 23:19:13 -0500

    Update DailyOperationsPage.tsx

--- frontend/src/pages/DailyOperationsPage.tsx
+++ frontend/src/pages/DailyOperationsPage.tsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
+import { AnimationGrid } from '../components/AnimationGrid';
+import colorConfigs from '../configs/colorConfigs'
+import { Accordion, AccordionSummary, AccordionDetails, Button, Breadcrumbs, Box, Typography, Container, Grid } from '@mui/material';
 import { TMoment, TMovement, TVehicle, VehicleType } from '../test/movements';
 import AlgorithmService from '../services/AlgorithmService';
 
@@ -6,22 +10,46 @@ export const DailyOperationsPage = () => {
 
   const [vehicles, setVehicles] = useState<TVehicle[]|undefined>(undefined);
   const [count, setCount] = useState(0);
+  const [apiMoment, setApiMoment] = useState<TMoment|undefined>(undefined);
   var time = new Date();
+  var started = true;
   const seconds = time.getHours()*60*60+time.getMinutes()*60+time.getSeconds();
-
+  
   const parseRoutes = (vehicles: TVehicle[]) => {
     let newVehicles = vehicles.map((v, index) => {
       if (v.type == VehicleType.auto) {
-        
+        v.id
       }
       return v;
     });
     return newVehicles;
   }
 
+  const parseApiMoment = (vehicles: TVehicle[]) => {
+    let moment: TMoment;
+    //moment = TMoment();
+    //moment.activeVehicles = vehicles.map(vehicle => {
+      //return vehicle;
+    //});
+    //return moment;
+  }
+
+  const openVehiclePopup = (vehicle: TVehicle) => {
+    console.log(vehicle);
+  }
+
   const planTheRoutes = async() => {
     await AlgorithmService.planRoutes(seconds).then((response) => {
-      setVehicles(parseRoutes(response.data));
+      //setVehicles(parseRoutes(response.data));
+      //setApiMoment(parseApiMoment(response.data));
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  const initiateAlgorithm = async() => {
+    await AlgorithmService.initDaily().then((response) => {
+      console.log('Algorithm initiated successfully');
     }).catch((err) => {
       console.log(err);
     });
@@ -35,9 +63,48 @@ export const DailyOperationsPage = () => {
     }, 60000);
   }, []);
 
+  useEffect(() => {
+    if(count==0 && started){
+      started=false;
+      initiateAlgorithm;
+    }
+  }, []);
+
   return (
     <>
-    <h1>The component has been rendered for {count} minutes {seconds}</h1>
+      <Breadcrumbs 
+        maxItems={2} 
+        aria-label='breadcrumb'
+        sx={{
+          paddingLeft: '10px',
+          paddingRight: '10px',
+          paddingTop: '5px',
+          paddingBottom: '5px',
+          backgroundColor: colorConfigs.breadcrumb.bg
+        }}
+      >
+          <Typography>Visualización</Typography>
+      </Breadcrumbs>
+
+      <Container>
+        <Box sx={{ display: 'flex' }}>
+          <Box
+            sx={{
+              padding: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            <AnimationGrid 
+              moment = {undefined}
+              openVehiclePopup={openVehiclePopup}
+            />
+          </Box>
+        </Box>
+      </Container>
+
+      <h1>The component has been rendered for {count} minutes. {seconds} seconds since beggining of day</h1>
     </>
   )
 }
